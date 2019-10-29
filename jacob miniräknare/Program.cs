e4a9f5fa0a28c5a919b4ad4c901675b6db2efc65
jacob jolsö <jacke_j_1998@hotmail.se>
2019-10-29 15:57:50 +0100

    Add project files.

---
+++ jacob miniräknare/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace jacob_miniräknare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum;
+            int secondNum;                   
+            string operation;
+            int answer;
+
+            Console.WriteLine("Welcome!");
+            Console.WriteLine("press Enter to start or -1 to exit");
+            Console.ReadLine();
+
+            Console.Write("Enter the first number in your basic equation: ");
+            firstNum = Convert.ToInt32(Console.ReadLine());
+
+            //
+            Console.Write("Ok now enter your operation ( x , / , +, -) ");
+            operation = Console.ReadLine();
+            Console.Write("Now enter your second number in the basic equation: ");
+            secondNum = Convert.ToInt32(Console.ReadLine());
+            //fixa en loop här!!
+            
+            bool keepAlive = true;
+            while (keepAlive)
+            {
+
+                try
+                {
+
+
+            if (operation == "x")
+            {
+                answer = firstNum * secondNum;
+                Console.WriteLine(firstNum + " x " + secondNum + " = " + answer);
+                Console.ReadLine();
+            }
+            else if (operation == "/")
+            {
+                answer = firstNum / secondNum;
+                Console.WriteLine(firstNum + " / " + secondNum + " = " + answer);
+                Console.ReadLine();
+            }
+            //
+            else if (operation == "+")
+            {
+                answer = firstNum + secondNum;
+                Console.WriteLine(firstNum + " + " + secondNum + " = " + answer);
+                Console.ReadLine();
+            }
+            else if (operation == "-")
+            {
+                answer = firstNum - secondNum;
+                Console.WriteLine(firstNum + " - " + secondNum + " = " + answer);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Sorry that is not correct format! Please restart!");     //
+                Console.ReadLine();
+            }
+            if (keepAlive == false "-1");
+                
+                }
+
+            }
+
+
+        }
+    }
+}
